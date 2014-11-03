fd12f14e4b5a59d38c09d80fd7b5197ce46a43c3
Imran Ahmed <imran.nappa@gmail.com>
2014-11-04 11:23:22 +1300

    Update start.sh

--- prerender/templates/default/start.sh
+++ prerender/templates/default/start.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
+
+# preemptively killall node and phantomjs 
+killall  node phantomjs
+
 export RESOURCE_DOWNLOAD_TIMEOUT=1200
 export JS_TIMEOUT=1200
 export PORT=80
+export lockfile="prerender.lock"
+if [ ! -e $lockfile ]; then
+   trap "rm -f $lockfile; exit" INT TERM EXIT
+   touch $lockfile
+   
+   cd /root/prerender
+   source env.sh
+   npm install
+   node server.js
+   
+   rm -f $lockfile
+   trap - INT TERM EXIT
+else
+   echo "prerender is already running.... :("
+fi
 
-cd /root/prerender
-source env.sh
-npm install
-node server.js
